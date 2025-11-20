b6ad4d56dffaa3d76494c9dfa98b40bd6e40db21
Ameyk <144281165+Amey1005@users.noreply.github.com>
2025-11-21 04:37:54 +0530

    Add files via upload

---
+++ weather_day1.py
@@ -0,0 +1,23 @@
+temps = [30, 32, 29, 33, 35, 28, 31]
+print("Max:", max(temps))
+print("Min:", min(temps))
+print("Average:", sum(temps)/len(temps))
+
+fahrenheit = []
+
+for c in temps:
+    f = (c * 9/5) + 32
+    fahrenheit.append(f)
+
+print(fahrenheit)   # <- ONLY this print is outside
+
+today = {    # <- No indentation here
+    "city": "Hyderabad",
+    "temp_C": 31,
+    "humidity": 72,
+    "wind_kmph": 14
+}
+
+today["temp_F"] = (today["temp_C"] * 9/5) + 32
+
+print(today)
